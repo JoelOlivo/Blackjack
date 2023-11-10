389987d502f91fb5c968c4f296e663dfdd50c8e8
JoelOlivo <139314609+JoelOlivo@users.noreply.github.com>
2023-11-09 21:45:19 -0500

    agregar funcion pedir una carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -17,11 +17,42 @@ const crearBaraja = () => {
             baraja.push(especial + tipo);        
         }
     }
-    
-    console.log(baraja);
-    console.log(_.shuffle(baraja));
 
-    return _.shuffle(baraja);
+    // console.log(baraja);
+    baraja = _.shuffle(baraja);
+    console.log(`mi baraja: ${baraja}`);
+
+    return baraja;
 }
 
 crearBaraja();
+
+const perdirCarta = () => {
+    
+    // Lo que yo hice
+    // let miCarta;
+    // let totalBaraja = baraja.length;
+    // miCarta = baraja[totalBaraja - 1];
+    
+    // baraja.pop();
+    // console.log(miCarta);
+    // console.warn('Nueva baraja:', baraja);
+
+    // return miCarta;
+
+    // Como hizo el profe xd
+    if (baraja.length === 0) {
+        throw 'Se acabaron las cartas'; // 'trow' permite mostrar un error en consola y detener el programa
+        // alert('¡Se acabaron tus cartas! :(');
+    } 
+
+    let miCarta = baraja.pop();
+    console.log(miCarta);
+    console.warn('Nueva baraja:', baraja);
+    return miCarta;
+    
+}
+
+// perdirCarta();
+    
+
